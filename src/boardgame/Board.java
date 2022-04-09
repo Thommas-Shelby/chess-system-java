71708c9a55e57e53e4a02d3476742467b1e4146d
Thommas Shelby <jordan-360@live.com>
2022-04-09 04:48:30 -0300

    Board end Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,63 @@
+package boardgame;
+
+public class Board {
+	private int rows;
+	private int columns;
+	
+	private Piece[][] pieces;
+	
+	//constructor
+ 	public Board () {
+		
+	}
+	
+	public Board(int rows, int columns, Piece[][] pieces) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns]; 
+		
+	}
+
+	//Get's Set
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+	//methods
+	public Piece piece (int rows , int columns) {
+		
+	}
+	
+	public Piece piece (Position position) {
+		
+	}
+	
+	public void placePiece(Piece piece , Position position) {
+		
+	}
+	
+	public Piece removePiece(Position position) {
+		
+	}
+	
+	public Boolean positionExists(Position position) {
+		
+	}
+	
+	public Boolean thereIsAPiece(Position position) {
+		
+	}
+
+}
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,44 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	
+	private Board board;
+	
+	//constructor
+	public Piece( ) {
+		
+	}
+	
+
+	
+	public Piece(Position position, Board board) {
+		this.board = board;
+	}
+
+
+
+	//Get's Set
+	public Position getPosition() {
+		return position;
+	}
+
+	
+	
+	protected Board getBoard() {
+		return board;
+	}
+
+	//methods
+	public Boolean possibleMoves() {
+		
+	}
+	public Boolean possibleMove(Position position) {
+		
+	}
+	public Boolean isThereAnyPossibleMove() {
+		
+	}
+	
+}
